c6310f2e6f91ba7f1f9020fb6a07363ef02d8be4
carolin.labinski <carolin.labinski@yahoo.de>
2020-02-18 23:32:37 +0100

    ajout seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,42 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# User.destroy_all
+# Event.destroy_all
+# Attendance.destroy_all
+
+# 10.times do |index|
+#     User.create!(
+#       email: Faker::Internet.email(domain: 'yopmail'), 
+#       )
+#   end
+
 User.destroy_all
 Event.destroy_all
-Attendance.destroy_all
 
-10.times do |index|
-    User.create!(
-      email: Faker::Internet.email(domain: 'yopmail'), 
-      )
-  end
+b=1
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    description: Faker::Lorem.paragraph(sentence_count: 3),
+    email: Faker::Name.first_name.downcase + b.to_s + "@yopmail.com",
+    password: "mdpmdp"
+  )
+  b+=1
+end
+
+
+multiple_of_five = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
+a=1
+20.times do
+  Event.create!(
+    start_date: Faker::Time.between(from: DateTime.now, to: DateTime.now+30),
+    duration: multiple_of_five.sample,
+    title: "Fake Event " + a.to_s,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.between(from: 20, to: 1000),
+    location: Faker::Address.city,
+  )
+a+=1
+end
